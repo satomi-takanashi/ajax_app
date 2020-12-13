ca71e108d31360eaa2323fa99bd18858a38b90ed
satomi-takanashi <74594934+satomi-takanashi@users.noreply.github.com>
2020-12-13 20:05:48 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController 
+ 
   def index 
     @posts = Post.all.order(id: "DESC")
   end
+  
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
+
   def checked
     post = Post.find(params[:id])
     if post.checked
