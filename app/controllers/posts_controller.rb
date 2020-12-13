e939e3a4f943df4a9723fd5fef87a48ff8445d58
satomi-takanashi <74594934+satomi-takanashi@users.noreply.github.com>
2020-12-13 21:27:07 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def index 
     @posts = Post.all.order(id: "DESC")
   end
-  
+
   def create
     post = Post.create(content: params[:content], checked: false)
     render json:{ post: post }
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   def checked
     post = Post.find(params[:id])
     if post.checked
-      post.update(checed: false)
+      post.update(checked: false)
     else
       post.update(checked: true)
     end
